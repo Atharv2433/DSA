5f089195d79b150ceca12f51afd6ea5b676c17e4
Atharv Joundal <158443883+Atharv2433@users.noreply.github.com>
2024-10-11 20:05:13 +0530

    Add files via upload

---
+++ Bit.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Bit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // Scanner to read input
+        int n = sc.nextInt(); // Number of operations
+        int x = 0; // Initialize x to 0
+
+        while (n-- > 0) { // Loop n times
+            String s = sc.next(); // Read each operation as a string
+            if (s.charAt(1) == '+') { // Check if the second character is '+'
+                ++x; // Increment x
+            } else {
+                --x; // Decrement x
+            }
+        }
+
+        System.out.println(x); // Output the final value of x
+        sc.close(); // Close the scanner
+    }
+}
