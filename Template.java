4486b727ee766bd65c183ef235d8533f92a50cc6
Atharv Joundal <158443883+Atharv2433@users.noreply.github.com>
2024-10-27 22:07:15 +0530

    Add files via upload

--- Template.java
+++ Template.java
@@ -1,72 +1,36 @@
-package Topological_Sort;
+package Heap;
 
-import java.util.ArrayList;
+import java.util.PriorityQueue;
 import java.util.Scanner;
-import java.util.Stack;
 
 public class Template {
 
-    public static class Pair{
-        int src , dest;
-        public Pair(int src ,int dest){
-            this.src = src;
-            this.dest = dest;
-        }
-        public int getFirst(){
-            return src;
-        }
-    }
-
-
-    public void topo_Dfs(int start , boolean vis[] , Stack<Integer> st){
-        vis[start] = true;
-        st.push(start);
-
-        for(Pair it : adj.get(start)){
-            if(!vis[it.getFirst()]){
-                topo_Dfs(it.getFirst(),vis,st);
-            }
-        }
-    }
-    
-
-   ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
-    public void addEdge(int src , int dest){
-        adj.get(src).add(new Pair(src, dest));
-        adj.get(dest).add(new Pair(dest, src));
-    }
-    
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt(); 
-        int m = sc.nextInt();
+        int n = sc.nextInt();
 
-        Template t = new Template();
-
-        for(int i = 0; i < m; i++){
-            int src = sc.nextInt();
-            int dest = sc.nextInt();
-
-            t.addEdge(src, dest);
-
-        }
-
-        boolean vis[] = new boolean[n];
-        Stack st = new Stack<>();
+        int arr[] = new int[n];
 
         for(int i = 0; i < n; i++){
-            if(!vis[i]){
-                t.topo_Dfs(i,vis,st);
+            arr[i] = sc.nextInt();
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for(int i = 0; i < n; i++){
+            pq.offer(arr[i]);
+
+            if(pq.size() > 2){
+                pq.poll();
             }
         }
 
-        if(st.size() == n){
-            System.out.println("No Cycle");
-        }else{
-            System.out.println("Cycle");
+        while (!pq.isEmpty()) {
+            System.out.println(pq.poll());
         }
         
     }
+    
 }
